30a1c20ba6d6523ac002a1d3775d32cafb380889
priyamoini <35995884+priyamoini@users.noreply.github.com>
2018-02-05 16:04:30 +0530

    ap.c
    progaram

---
+++ ap.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+    int N, i;
+    factorial N<=20;
+
+    printf("Enter an integer: ");
+    scanf("%d",&n);
+    if (N<=20)
+        printf("Error! Factorial of a negative number doesn't exist.");
+
+    else
+    {
+        for(i=1; i<=n; ++i)
+        {
+            factorial *= i;           
+        }
+        printf("Factorial of %d = %llu", N, factorial);
+    }
+
+    return o;
+    }
