eb06cd89b23d64c143bc04bbeb9d60b68197e0c2
priyamoini <35995884+priyamoini@users.noreply.github.com>
2018-02-05 16:33:44 +0530

    number.c
    progaram

---
+++ number.c
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+void appendDemo(string str1, string str2)
+{
+ append(str);
+    cout << "Using append() : ";
+}
+int main()
+{
+    string str("ammu");
+    cout << "Original String : " << str << endl;
+    return 0;
+}
