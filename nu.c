de2dbc323abef2096661b85c056660757313a859
priyamoini <35995884+priyamoini@users.noreply.github.com>
2018-02-09 16:53:44 +0530

    nu.c
    progaram

---
+++ nu.c
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+void appendDemo(string str1,str2)
+{
+ append(str1,str2);
+    cout << "Using append() : ");
+}
+int main()
+{
+    string str1("priya");
+ string str2("ammu");
+    cout << "Original String : " << str1<<str2 << endl;
+    return 0;
+}
