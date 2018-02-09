a2581482047b8cc2467e1229410e0c1105438b4a
priyamoini <35995884+priyamoini@users.noreply.github.com>
2018-02-09 16:40:53 +0530

    srr.c
    progaram

---
+++ srr.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<string>
+void main()_
+{
+int a,b;
+printf("entre the value of even number:");
+scanf("%d",&a);
+printf("enter the value of odd number:");
+scanf("%d",&b)
+getch();
+}
