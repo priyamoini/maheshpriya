040f92a75117dd6a332d9f2c9087475354396f21
priyamoini <35995884+priyamoini@users.noreply.github.com>
2018-02-12 15:48:29 +0530

    array12.c
    program

---
+++ array12.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+int arr[10],i,n;
+printf("\n enter n element");
+scanf"%d",&n);
+for(i=0;i<n;i++)
+{
+printf("enter arr[%a]=",i);
+scanf("%d",&arr[i];
+}
+for(i=0;i<n;i++)
+{
+printf(%d\n",arr[i]);
+}
+}
