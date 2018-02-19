e3217a54dcf7b35e86c04bb5c1e0a37bf8b83461
priyamoini <35995884+priyamoini@users.noreply.github.com>
2018-02-19 16:13:32 +0530

    3point19.c
    program

---
+++ 3point19.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void collinear(int x1,int y1,int x2,int y2,int x3,int y3);
+{
+	int a=x1* (y2-y1) + x2* (y3-y2) + x3* (y1-y3);
+	int(a==0)
+	printf("yes");
+	else
+	printf("no");
+}
+int main()
+{
+	int a=x1=1 ,y1=3 ,x2=2 ,y2=1 ,x3=0 ,y3=1;
+	collinear=(x1, x2, x3, y1, y2, y3);
+	return 0;
+}
+
