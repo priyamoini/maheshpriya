ab2079aafd3c2eb1c2e759fcd3751bf16e594ae4
priyamoini <35995884+priyamoini@users.noreply.github.com>
2018-02-19 16:40:43 +0530

    k num19.c
    program

---
+++ k num19.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int arr[10],1,n;
+printf("enter N element");
+sacnf("%d",&n);
+for(i=0;i<n;i++);
+{
+printf("enter arr[%a]=");
+scanf(%d",&arr[i]);
+}
+for(i=0;i<n;i+=)
+{
+printf("%d",&arr[i]);
+return 0;
+}
+}
