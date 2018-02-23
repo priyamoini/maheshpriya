4d4e563037388ef744a8442a57d4e0d24aeb2a55
priyamoini <35995884+priyamoini@users.noreply.github.com>
2018-02-23 16:54:58 +0530

    number23.c
    program

---
+++ number23.c
@@ -0,0 +1,15 @@
+#include<stdio.c>
+void main()
+{
+int i,n;/
+printf("enter all even numder till:");
+scanf("%d",&n);
+printf("all even numbers  1 to %d are:\n",n);
+i=5;
+while(i<n)
+{
+printf("%d\n",i);
+i+=5;
+}
+return 0;
+}
