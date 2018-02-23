afcbd2761385b3ae8514c835c3267cafa4f3e881
priyamoini <35995884+priyamoini@users.noreply.github.com>
2018-02-23 15:46:39 +0530

    space23.c
    program

---
+++ space23.c
@@ -0,0 +1,18 @@
+#include<stdio.c>
+int void mainn()
+{
+int a,b,c;
+printf("enter the numder:"a);
+sacnf("%d",&a);
+printf("enter the number:"b);
+sacnf("%d",&b);
+printf("enter the number:"c);
+scanf("%d",&c);
+for(i=0;i<=5;i++)
+{
+printf("yes");
+else
+printf("no");
+}
+return 0;
+}
