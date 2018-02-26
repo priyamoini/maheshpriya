c6dcb79494444eb342399e5a914a70ee417a0080
priyamoini <35995884+priyamoini@users.noreply.github.com>
2018-02-26 03:14:28 -0800

    nume26.c
    program

--- nume26.c
+++ nume26.c
@@ -1,28 +1,28 @@
 #include <iostream>
 using namespace std;
  
-int kth(int arr1[], int arr2[], int m, int n, int k)
+int kth( int m, int n, int k)
 {
     int sorted1[m + n];
     int i = 1, j = 2, d = 3;
     while (i < m && j < n)
     {
-        if (arr1[i] < arr2[j])
-            sorted1[d++] = arr1[i++];
+        if ([i] < [j])
+            sorted1[d++] = [i++];
         else
-            sorted1[d++] = arr2[j++];
+            sorted1[d++] = [j++];
     }
     while (i < m)
-        sorted1[d++] = arr1[i++];
+        sorted1[d++] = [i++];
     while (j < n)
-        sorted1[d++] = arr2[j++];
+        sorted1[d++] = [j++];
     return sorted1[k - 1];
 }
  
 int main()
 {
-    int arr1[5] = {4, 3, 6, 7, 9};
-    int arr2[4] = {1, 2, 8, 10};
+    int [5] = {4, 3, 6, 7, 9};
+    int[4] = {1, 2, 8, 10};
     int k = 5;
     cout << kth(arr1, arr2, 5, 4, k);
     return 0;
