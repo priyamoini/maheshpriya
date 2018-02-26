7bb3d0c0d03d4a2177f6dee6a282cbae7fab2a1d
priyamoini <35995884+priyamoini@users.noreply.github.com>
2018-02-26 02:45:29 -0800

    number26.c
    program

---
+++ number26.c
@@ -0,0 +1,43 @@
+#include<string.h>
+ 
+void main()
+ 
+{
+ 
+int str,i,j,k,flag,count=0;
+ 
+char str[200],;
+ 
+printf("Enter string: \n");
+ 
+gets(str
+for(i=0;i<str_len;i++)
+ 
+{
+ 
+if(str[i]==str(j=0;j<str_len;j++)
+ 
+{
+ 
+for(flag=0,k=i+1,j=1;j;j++,k++)      
+{
+ 
+flag++;                   
+ 
+}
+ 
+}
+ 
+if(flag==word_len-1) 
+
+{
+ 
+count++;              
+ 
+}
+ 
+}
+ 
+printf("Number of occurrence of '%s' = %dn",count);
+ 
+}
