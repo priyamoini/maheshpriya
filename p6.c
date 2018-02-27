187cd0a12b1acd059125e65d72400e852246528f
priyamoini <35995884+priyamoini@users.noreply.github.com>
2018-02-27 12:57:24 +0530

    Create p6.c

---
+++ p6.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(void) {
+int year;
+ scanf("%d",&year);
+ if(year%4==0)
+ {
+ printf("It is a leap year");
+ }
+ else
+ {
+ printf("It is not a leap year");
+ }
+return 0;
