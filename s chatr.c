bf153c5a340e8bb125e0795e1dc4557acdd1f733
priyamoini <35995884+priyamoini@users.noreply.github.com>
2018-03-02 02:16:01 -0800

    s chart.c
    program

---
+++ s chatr.c
@@ -0,0 +1,21 @@
+#include<stdio.c>
+viod main()
+{
+int a,b,s,i;
+str(s)
+printf("enter the value of string s");
+scanf("%d",&s)
+printf("entre the value of:"a);
+scanf("%d",&a);
+printf("enter the value of:"b);
+scanf("%d",&b);
+for(i=0;i<5;i++)
+{
+if(i++)
+{
+printf("enter the string value:"s);
+}
+return 0;
+}
+}
+
