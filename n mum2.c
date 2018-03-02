d01587f2bfd1abee2f6f5af15161ba4188ed9cad
priyamoini <35995884+priyamoini@users.noreply.github.com>
2018-03-02 03:26:15 -0800

    n num2.c
    program

---
+++ n mum2.c
@@ -0,0 +1,13 @@
+#include<stsio.h>
+void main()
+{
+int i,n;
+pritf('enter the odd number till:");
+scanf("%d",&n);
+printf("all the odd numbers 1 to %d from:")
+for(i=0;i<5;i++)
+{
+printf("all the odd numbers=%d");
+}
+return 0;
+}
